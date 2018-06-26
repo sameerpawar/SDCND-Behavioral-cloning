e80be16931267c08eaf140cf6d62d3ef3e310e7c
Sameer Pawar <sameeronnet@gmail.com>
2018-06-26 13:49:59 -0700

    working(mostly LeNet) files not regularized

--- CNN_models.py
+++ CNN_models.py
@@ -1,53 +1,16 @@
 # Behavioral Cloning Neural Network model (BCNN)
+import tensorflow as tf
 from keras.layers import Input, Flatten, Dense, Conv2D, Add, Lambda, Cropping2D, MaxPooling2D
 from keras.layers import BatchNormalization, Activation, Dropout, Concatenate
 from keras.models import Model
  
- 
-def LeNet(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
-    dropout_rate = c_drop_rate + fc_drop_rate
-    if dropout_rate > 0:
-        DropoutFlag = True
-    else:
-        DropoutFlag = False
-
-    # Vanilla LeNet
-    inp   = Input(shape=input_shape)
-    cropped_inp   = Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))(inp)
-    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
-     
-    # convolution (5, 5, inputchannels, 6)
-    conv1 = Conv2D(6, 5, padding='valid', activation='relu')(norm_inp) 
-    if BN:
-        conv1   = BatchNormalization()(conv1)
-    if DropoutFlag:
-        conv1     = Dropout(c_drop_rate)(conv1)
-    maxp1 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv1)
- 
-    # convolution (5, 5, inputchannels, 16)
-    conv2 = Conv2D(16, 5, padding='valid', activation='relu')(maxp1)
-    if BN:
-        conv2 = BatchNormalization()(conv2)    
-    if DropoutFlag:
-        conv2     = Dropout(c_drop_rate)(conv2)        
-    maxp2 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv2)
- 
-    fc1_inp  = Flatten()(maxp2)    
-    fc1  = Dense(120, activation='relu')(fc1_inp)
-    if BN:
-        fc1 = BatchNormalization()(fc1)    
-    if DropoutFlag:
-        fc1 = Dropout(fc_drop_rate)(fc1) 
- 
-    fc2  = Dense(84, activation='relu')(fc1)
-    if BN:
-        fc2 = BatchNormalization()(fc2)
-    if DropoutFlag:
-        fc2 = Dropout(fc_drop_rate)(fc2)
-
-    out = Dense(1)(fc2)
-    model = Model(inp, out)
-    return model
+'''
+def normalized_hsv (x):
+    h, s, v     = tf.split(tf.image.rgb_to_hsv(x), [1, 1, 1], 2)
+    v           = tf.div(v, tf.constant(255.0))
+    hsv_img     = tf.concat((h,s,v),2)
+    return tf.subtract(hsv_img, tf.constant(0.5))
+'''
  
 def AlexNet(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
     dropout_rate = c_drop_rate + fc_drop_rate
@@ -155,7 +118,7 @@ def AlexNet(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
         fc5 = Dropout(fc_drop_rate)(fc5)    
  
     fc6         = Dense(4096, activation='relu')(fc5)
-     if BN:
+    if BN:
         fc6 = BatchNormalization()(fc6)
     if DropoutFlag:
         fc6 = Dropout(fc_drop_rate)(fc6)   
@@ -166,27 +129,19 @@ def AlexNet(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
     return model
  
 
-def hsv_conversion(x):
-    import tensorflow as tf    
-    return tf.image.rgb_to_hsv(x)
-
-model.add(Lambda(hsv_conversion, input_shape=(160, 320, 3)))
-
-
 def Nvidia(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
-    # Add RGB to YUV
-    # Use left and right images
-
     dropout_rate = c_drop_rate + fc_drop_rate
     if dropout_rate > 0:
         DropoutFlag = True
     else:
         DropoutFlag = False
- 
-    inp   = Input(shape=input_shape)
-    cropped_inp   = Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))(inp)
-    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
-     
+
+    inp         = Input(shape=input_shape)
+    cropped_inp = Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))(inp)
+    #resized_inp     = Lambda(lambda x: tf.image.resize_images(x, (200,66)))(cropped_inp)
+    #norm_inp    = Lambda(lambda x: normalized_hsv(x))(resized_inp)
+    norm_inp    = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+
     conv1  = Conv2D(24, 5, strides=(2, 2), padding='valid', activation='relu')(norm_inp)
     if BN:
         conv1   = BatchNormalization()(conv1)
@@ -207,7 +162,7 @@ def Nvidia(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
         conv3     = Dropout(c_drop_rate)(conv3)
  
 
-   conv4  = Conv2D(64, 3, strides=(1, 1), padding='valid', activation='relu')(conv3)
+    conv4  = Conv2D(64, 3, strides=(1, 1), padding='valid', activation='relu')(conv3)
     if BN:
         conv4   = BatchNormalization()(conv4)
     if DropoutFlag:
@@ -225,8 +180,8 @@ def Nvidia(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
         fc6 = BatchNormalization()(fc6)
     if DropoutFlag:
         fc6 = Dropout(fc_drop_rate)(fc6)
-
-    fc7         = Dense(100, activation='relu')(fc6)
+    
+    fc7         = Dense(100, activation='relu')(fc6)    
     if BN:
         fc7 = BatchNormalization()(fc7)
     if DropoutFlag:
@@ -248,11 +203,285 @@ def Nvidia(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
     model = Model(inp, out)
     return model
 
+
+def VGG_16(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
+    inp         = Input(shape=input_shape)
+    cropped_inp = Cropping2D(cropping=((52,20), (0,0)), input_shape=(160,320,3))(inp)
+    norm_inp    = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+    
+    # i/p 88 x 320 x 3  
+    conv1_1     = Conv2D(64, 3, strides=(1, 1), padding='same', activation='relu')(norm_inp)
+    conv1_2     = Conv2D(64, 3, strides=(1, 1), padding='same', activation='relu')(conv1_1)
+    maxpool_1   = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv1_2)
+
+    # i/p 44 x 160 x 64
+    conv2_1     = Conv2D(128, 3, strides=(1, 1), padding='same', activation='relu')(maxpool_1)
+    conv2_2     = Conv2D(128, 3, strides=(1, 1), padding='same', activation='relu')(conv2_1)
+    maxpool_2   = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv2_2)
+
+    # i/p 22 x 80 x 128
+    conv3_1     = Conv2D(256, 3, strides=(1, 1), padding='same', activation='relu')(maxpool_2)
+    conv3_2     = Conv2D(256, 3, strides=(1, 1), padding='same', activation='relu')(conv3_1)
+    conv3_3     = Conv2D(256, 3, strides=(1, 1), padding='same', activation='relu')(conv3_2)
+    maxpool_3   = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv3_3)
+
+    # i/p 11 x 40 x 256
+    conv4_1     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(maxpool_3)
+    conv4_2     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(conv4_1)
+    conv4_3     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(conv4_2)
+    maxpool_4   = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv4_3)
+
+    # i/p 6 x 20 x 512
+    conv5_1     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(maxpool_4)
+    conv5_2     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(conv5_1)
+    conv5_3     = Conv2D(512, 3, strides=(1, 1), padding='same', activation='relu')(conv5_2)
+    maxpool_5   = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(conv5_3)
+
+    # i/p 3 x 10 x 512 = 15360
+    fc1_inp     = Flatten()(maxpool_5)
+    # Parameters = 15 k *1 k = 15 million
+    fc1         = Dense(4096, activation='relu')(fc1_inp)
+    # Parameters = 4 k *4 k = 16 million
+    fc2         = Dense(4096, activation='relu')(fc1)
+    out         = Dense(1, activation='relu')(fc2)
+    model       = Model(inp, out)
+    return model
+
  
-#Read on saved webpage about AlexNet finetuning.
+def LeNet(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
+    # Total parameters ~ 3.4 million
+    dropout_rate = c_drop_rate + fc_drop_rate
+    if dropout_rate > 0:
+        DropoutFlag = True
+    else:
+        DropoutFlag = False
+
+    # Input size = (160,320,3)
+    # Parameters = 0
+    inp   = Input(shape=input_shape)
+    cropped_inp   = Cropping2D(cropping=((52, 20), (0, 0)), input_shape=(160,320,3))(inp)
+    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+
+    # Input size = (88,320,3)
+    conv1 = Conv2D(6, 5, padding='same', activation='relu')(norm_inp) 
+    if BN:
+        conv1   = BatchNormalization()(conv1)
+    if DropoutFlag:
+        conv1     = Dropout(c_drop_rate)(conv1)
+    maxp1 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv1)
+
+    # Input size = (44,160,6)
+    conv2 = Conv2D(16, 5, padding='same', activation='relu')(maxp1)
+    if BN:
+        conv2 = BatchNormalization()(conv2)    
+    if DropoutFlag:
+        conv2 = Dropout(c_drop_rate)(conv2)        
+    maxp2 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv2)
+
+    # Input size = (22,80,16)
+    # Parameters = 22*80*16*120 + 120 ~ 3.38 million
+    fc1_inp  = Flatten()(maxp2)    
+    fc1  = Dense(120, activation='relu')(fc1_inp)
+    if BN:
+        fc1 = BatchNormalization()(fc1)    
+    if DropoutFlag:
+        fc1 = Dropout(fc_drop_rate)(fc1) 
+
+    # Input size = 120
+    fc2  = Dense(84, activation='relu')(fc1)
+    if BN:
+        fc2 = BatchNormalization()(fc2)
+    if DropoutFlag:
+        fc2 = Dropout(fc_drop_rate)(fc2)
+
+    # Input size = 84
+    out = Dense(1)(fc2)
+    model = Model(inp, out)
+    return model 
+
+
  
-'''
-ResNet
-VGG
-1. VGG or Inception classes in keras with pre-trained weights
-'''
+def LeNet_v0(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
+    # 2 conv but wider than LeNet
+    dropout_rate = c_drop_rate + fc_drop_rate
+    if dropout_rate > 0:
+        DropoutFlag = True
+    else:
+        DropoutFlag = False
+
+    # Input size = (160,320,3)
+    # Parameters = 0
+    inp   = Input(shape=input_shape)
+    cropped_inp   = Cropping2D(cropping=((52, 20), (0, 0)), input_shape=(160,320,3))(inp)
+    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+
+    # Input size = (88,320,3)
+    conv1 = Conv2D(16, 5, padding='valid', activation='relu')(norm_inp) 
+    if BN:
+        conv1   = BatchNormalization()(conv1)
+    if DropoutFlag:
+        conv1     = Dropout(c_drop_rate)(conv1)
+    maxp1 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv1)
+
+    # Input size = (42,158,16)
+    conv2 = Conv2D(32, 5, padding='valid', activation='relu')(maxp1)
+    if BN:
+        conv2 = BatchNormalization()(conv2)    
+    if DropoutFlag:
+        conv2 = Dropout(c_drop_rate)(conv2)        
+    maxp2 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv2)
+
+    # Input size = (19,77,32)
+    # Parameters = 19*77*32*120 + 120 ~ 5.6 million
+    fc1_inp  = Flatten()(maxp2)    
+    fc1  = Dense(120, activation='relu')(fc1_inp)
+    if BN:
+        fc1 = BatchNormalization()(fc1)    
+    if DropoutFlag:
+        fc1 = Dropout(fc_drop_rate)(fc1) 
+
+    # Input size = 120
+    fc2  = Dense(84, activation='relu')(fc1)
+    if BN:
+        fc2 = BatchNormalization()(fc2)
+    if DropoutFlag:
+        fc2 = Dropout(fc_drop_rate)(fc2)
+
+    # Input size = 84
+    out = Dense(1)(fc2)
+    model = Model(inp, out)
+    return model 
+
+
+def LeNet_v1(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
+    # 3 conv but wider than LeNet
+    dropout_rate = c_drop_rate + fc_drop_rate
+    if dropout_rate > 0:
+        DropoutFlag = True
+    else:
+        DropoutFlag = False
+
+    # Input size = (160,320,3)
+    # Parameters = 0
+    inp   = Input(shape=input_shape)
+    cropped_inp   = Cropping2D(cropping=((52, 20), (0, 0)), input_shape=(160,320,3))(inp)
+    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+
+    # Input size = (88,320,3)
+    conv1 = Conv2D(16, 5, padding='valid', activation='relu')(norm_inp) 
+    if BN:
+        conv1   = BatchNormalization()(conv1)
+    if DropoutFlag:
+        conv1     = Dropout(c_drop_rate)(conv1)
+    maxp1 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv1)
+
+    # Input size = (42,158,16)
+    conv2 = Conv2D(32, 5, padding='valid', activation='relu')(maxp1)
+    if BN:
+        conv2 = BatchNormalization()(conv2)    
+    if DropoutFlag:
+        conv2 = Dropout(c_drop_rate)(conv2)        
+    maxp2 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv2)
+
+    # Input size = (19,77,32)
+    conv3 = Conv2D(64, 3, padding='valid', activation='relu')(maxp2)
+    if BN:
+        conv3 = BatchNormalization()(conv3)    
+    if DropoutFlag:
+        conv3 = Dropout(c_drop_rate)(conv3)        
+    maxp3 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv3)
+
+
+    # Input size = (8,37,64)
+    # Parameters = 8*37*64*120 ~ 2.3 million
+    fc1_inp  = Flatten()(maxp3)    
+    fc1  = Dense(120, activation='relu')(fc1_inp)
+    if BN:
+        fc1 = BatchNormalization()(fc1)    
+    if DropoutFlag:
+        fc1 = Dropout(fc_drop_rate)(fc1) 
+
+    # Input size = 120
+    fc2  = Dense(84, activation='relu')(fc1)
+    if BN:
+        fc2 = BatchNormalization()(fc2)
+    if DropoutFlag:
+        fc2 = Dropout(fc_drop_rate)(fc2)
+
+    # Input size = 84
+    out = Dense(1)(fc2)
+    model = Model(inp, out)
+    return model 
+
+
+
+def LeNet_v2(input_shape, BN = False, c_drop_rate = 0, fc_drop_rate = 0):
+    # 4 conv but wider than LeNet
+    dropout_rate = c_drop_rate + fc_drop_rate
+    if dropout_rate > 0:
+        DropoutFlag = True
+    else:
+        DropoutFlag = False
+
+    # Input size = (160,320,3)
+    # Parameters = 0
+    inp   = Input(shape=input_shape)
+    cropped_inp   = Cropping2D(cropping=((52, 20), (0, 0)), input_shape=(160,320,3))(inp)
+    norm_inp = Lambda(lambda x: x/255.0 - 0.5)(cropped_inp)
+
+    # Input size = (88,320,3)
+    conv1 = Conv2D(16, 5, padding='valid', activation='relu')(norm_inp) 
+    if BN:
+        conv1   = BatchNormalization()(conv1)
+    if DropoutFlag:
+        conv1     = Dropout(c_drop_rate)(conv1)
+    maxp1 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv1)
+
+    # Input size = (42,158,16)
+    conv2 = Conv2D(32, 5, padding='valid', activation='relu')(maxp1)
+    if BN:
+        conv2 = BatchNormalization()(conv2)    
+    if DropoutFlag:
+        conv2 = Dropout(c_drop_rate)(conv2)        
+    maxp2 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv2)
+
+    # Input size = (19,77,32)
+    conv3 = Conv2D(64, 3, padding='valid', activation='relu')(maxp2)
+    if BN:
+        conv3 = BatchNormalization()(conv3)    
+    if DropoutFlag:
+        conv3 = Dropout(c_drop_rate)(conv3)        
+    maxp3 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv3)
+
+
+    # Input size = (8,37,64)
+    conv4 = Conv2D(64, 3, padding='valid', activation='relu')(maxp3)
+    if BN:
+        conv4 = BatchNormalization()(conv4)    
+    if DropoutFlag:
+        conv4 = Dropout(c_drop_rate)(conv4)        
+    maxp4 = MaxPooling2D(pool_size=(2, 2), strides = (2, 2))(conv4)
+
+
+    # Input size = (3,17,64)
+    # Parameters = 3*17*64*120 ~ 0.4 million
+    fc1_inp  = Flatten()(maxp4)    
+    fc1  = Dense(120, activation='relu')(fc1_inp)
+    if BN:
+        fc1 = BatchNormalization()(fc1)    
+    if DropoutFlag:
+        fc1 = Dropout(fc_drop_rate)(fc1) 
+
+    # Input size = 120
+    fc2  = Dense(84, activation='relu')(fc1)
+    if BN:
+        fc2 = BatchNormalization()(fc2)
+    if DropoutFlag:
+        fc2 = Dropout(fc_drop_rate)(fc2)
+
+    # Input size = 84
+    out = Dense(1)(fc2)
+    model = Model(inp, out)
+    return model 
+
+
--- model.py
+++ model.py
@@ -7,21 +7,19 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
-from BCNNmodel import LeNet, LeNet_D, LeNet_BN
-from BCNNmodel import AlexNet, AlexNet_v1
-#from BCNNmodel import Nvidia, Nvidia_D, Nvidia_BN
-#from BCNNmodel import ResNet
+from keras.optimizers import Adam, SGD
+from CNN_models import LeNet, LeNet_v0, LeNet_v1, LeNet_v2
+from CNN_models import Nvidia
+
 
 #Course data 0 - 8036
 # two laps of track 1: 8037-12618
 # two laps of recovery data track-1: 
-
 #**************************************************************************************************************
 def read_data_fun(samples, src):
     images = [] # X_data
     angles = [] # y_data
     correction = 0.2 # this is a parameter to tune
-
     for line in samples:
         source_path = line[0]
         center_filename = source_path.split('/')[-1]
@@ -50,8 +48,7 @@ def read_data_fun(samples, src):
                 angle_flipped = -left_angle
                 images.append(image_flipped)
                 angles.append(angle_flipped)
-
-
+                
             source_path = line[2]
             right_filename = source_path.split('/')[-1]
             current_path = './' + src + '/IMG/' + right_filename
@@ -59,13 +56,12 @@ def read_data_fun(samples, src):
             right_angle = center_angle - correction
             images.append(right_image)
             angles.append(right_angle)            
-
             if FLIP:
                 image_flipped = np.fliplr(right_image)
                 angle_flipped = -right_angle
                 images.append(image_flipped)
                 angles.append(angle_flipped)
-
+    
     X_data = np.array(images)[...,[2, 1, 0]]
     y_data = np.array(angles)
     return X_data, y_data
@@ -73,12 +69,72 @@ def read_data_fun(samples, src):
 #**************************************************************************************************************
 
 
+#**************************************************************************************************************
+'''
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            correction = 0.2 # this is a parameter to tune
+            for batch_sample in batch_samples:
+                name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)            
+
+                if FLIP:
+                    # Flip the center image
+                    image_flipped = np.fliplr(center_image)
+                    angle_flipped = -center_angle
+                    images.append(image_flipped)
+                    angles.append(angle_flipped)
+
+                if LRImages:
+                    # append left and right images as well
+                    name = './data/IMG/'+batch_sample[1].split('/')[-1]
+                    left_image = cv2.imread(name)
+                    left_angle = center_angle + correction
+                    images.append(left_image)
+                    angles.append(left_angle)            
+                    if FLIP:        
+                        # Flip the left image
+                        image_flipped = np.fliplr(left_image)
+                        angle_flipped = -left_angle
+                        images.append(image_flipped)
+                        angles.append(angle_flipped)
+
+                    name = './data/IMG/'+batch_sample[2].split('/')[-1]
+                    right_image = cv2.imread(name)
+                    right_angle = center_angle - correction
+                    images.append(right_image)
+                    angles.append(right_angle)            
+                    if FLIP:
+                        # Flip the right image        
+                        image_flipped = np.fliplr(right_image)
+                        angle_flipped = -right_angle
+                        images.append(image_flipped)
+                        angles.append(angle_flipped)
+            X_train = np.array(images)[...,[2, 1, 0]]
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+'''
+#**************************************************************************************************************
+
+
+
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-
 # command line flags
 flags.DEFINE_string('model', 'LeNet', "Model selection.")
 flags.DEFINE_string('data', 'data', "training data folder.")
+flags.DEFINE_string('out', 'model.h5', "Output file name.")
 flags.DEFINE_integer('start_line', 0, "Starting of the line from excel.")
 flags.DEFINE_integer('end_line', 10**20, "Ending of the line from excel.")
 flags.DEFINE_integer('plot', 0, "The visualization flag.")
@@ -86,7 +142,10 @@ flags.DEFINE_boolean('flip', False, "Flip images boolean.")
 flags.DEFINE_boolean('LR', False, "Add left-right images boolean.")
 flags.DEFINE_integer('epochs', 1, "The number of epochs.")
 flags.DEFINE_integer('batch_size', 32, "The batch size.")
-flags.DEFINE_integer('total_samples', 10**20, "Maximum number of samples to be used for Training.")
+flags.DEFINE_boolean('BN', False, "batch normalization.")
+flags.DEFINE_float('conv_drop', 0.0, "Drop rate for conv layers.")
+flags.DEFINE_float('fc_drop', 0.0, "Drop rate for fully connected layers.")
+flags.DEFINE_float('rate', 1e-3, "Learning rate for fully connected layers.")
 
 FLIP = FLAGS.flip
 LRImages = FLAGS.LR
@@ -94,20 +153,18 @@ LRImages = FLAGS.LR
 input_shape = (160, 320, 3)
 
 switcher = {
-        'LeNet':         LeNet(input_shape),   
-        'LeNet_BN':      LeNet_BN(input_shape),
-        'LeNet_D':       LeNet_D(input_shape),
-        'AlexNet':       AlexNet(input_shape),
-        'AlexNet_v1':    AlexNet_v1(input_shape)
+        'LeNet':        LeNet(input_shape, BN = FLAGS.BN,  c_drop_rate = FLAGS.conv_drop, fc_drop_rate = FLAGS.fc_drop),   
+        'LeNet_v0':     LeNet_v0(input_shape, BN = FLAGS.BN,  c_drop_rate = FLAGS.conv_drop, fc_drop_rate = FLAGS.fc_drop),   
+        'LeNet_v1':     LeNet_v1(input_shape, BN = FLAGS.BN,  c_drop_rate = FLAGS.conv_drop, fc_drop_rate = FLAGS.fc_drop),   
+        'LeNet_v2':     LeNet_v2(input_shape, BN = FLAGS.BN,  c_drop_rate = FLAGS.conv_drop, fc_drop_rate = FLAGS.fc_drop),
+        'Nvidia':       Nvidia(input_shape, BN = FLAGS.BN,  c_drop_rate = FLAGS.conv_drop, fc_drop_rate = FLAGS.fc_drop)
     }
-
 
 samples = []
 start_line = FLAGS.start_line
 end_line = FLAGS.end_line
 
 with open('./data/driving_log.csv') as csvfile:
-    ignore_header = True
     line_counter = 0
     reader = csv.reader(csvfile)
     for line in reader:
@@ -115,34 +172,55 @@ with open('./data/driving_log.csv') as csvfile:
             samples.append(line)
         line_counter += 1
 
-        
-
-nsamples = np.minimum(FLAGS.total_samples,len(samples))
-samples = samples[:nsamples]    
-X_data, y_data = read_data_fun(samples, FLAGS.data)
-
-
-# Read the data
-print("epochs = ", FLAGS.epochs)
-print("batch size = ", FLAGS.batch_size)
-
-
-
-
 
 # Train a model
-
-#argument = 'Nvidia_D_v2'
-print("Using model " + FLAGS.model)
-print()
 model = switcher.get(FLAGS.model, "Invalid model")
-#model = LeNet(input_shape)
-model.compile(optimizer='adam', loss='mse')
+optm = Adam(lr=FLAGS.rate)
+#optm  = SGD(lr=FLAGS.rate, momentum=0.9, decay=0.0, nesterov=True)
+model.compile(optimizer=optm, loss='mse')
+
+
+
+#**************************************************************************************************************
+# If no generator
+X_data, y_data = read_data_fun(samples, FLAGS.data)
 history_object = model.fit(x= X_data, y = y_data, batch_size = FLAGS.batch_size, 
 epochs = FLAGS.epochs,verbose=1, validation_split=0.2, shuffle=True)
-model.save('model.h5')
+#**************************************************************************************************************
 
 
+#**************************************************************************************************************
+'''
+# If generator
+train_generator         = generator(train_samples, FLAGS.batch_size)
+validation_generator    = generator(validation_samples, FLAGS.batch_size)
+# Train a model
+history_object = model.fit_generator(train_generator, steps_per_epoch =
+len(train_samples), validation_data = 
+validation_generator,
+validation_steps = len(validation_samples), 
+epochs=FLAGS.epochs, verbose=1)
+'''
+#**************************************************************************************************************
+
+
+model.save(FLAGS.out)
+print("epochs = ", FLAGS.epochs)
+print("batch size = ", FLAGS.batch_size)
+print("Using model " + FLAGS.model)
+print("Output filename = " + FLAGS.out)
+if FLIP:
+    print("Flip enabled")
+if FLAGS.BN:
+    print("BN enabled")
+if LRImages:
+    print("left-right images used")
+
+if FLAGS.conv_drop + FLAGS.fc_drop > 0:
+    print("Drop out used")
+
+print("Learning rate used = ", FLAGS.rate)
+    
 if FLAGS.plot > 0:
     ### plot the training and validation loss for each epoch
     plt.plot(history_object.history['loss'])
